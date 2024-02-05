d149f97950a2fb81ebd99b94a907b822abed617d
EvelynLi1116 <56941157+EvelynLi1116@users.noreply.github.com>
2024-02-04 20:41:53 -0800

    feat: upload requested file

---
+++ module4.py
@@ -0,0 +1,15 @@
+def integer_checker():
+    N = int(input('Please provide a positive integer number:'))
+    
+    l_ = []
+    for i in range(N):
+        l_.append(int(input('Please provide an integer number:')))
+        
+    checker = int(input('Please provide another integer number:'))
+    if checker in l_:
+        return l_.index(checker)
+    else:
+        return -1
+
+    
+test_call = integer_checker()
